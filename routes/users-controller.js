2fb8a571d1a91ca1685482b872adeda83378e029
Bruin324 <bruin324@gmAIL.COM>
2017-07-02 17:35:50 -0400

    validation functioning

--- routes/users-controller.js
+++ routes/users-controller.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../models');
+const expressValidator = require('express-validator');
+
+router.use(expressValidator());
 
 
 router.get('/login', (request, response) => {
@@ -20,21 +23,33 @@ router.post('/register', async (request, response) => {
     var email = request.body.email;
     var password = request.body.password;
     var cpwd = request.body.cpwd;
-    // validate email, password, confirm password
-    // request.checkbody()
-    var newUser = await models.users.create({ name: name, email: email, password: password });
-    var userSession = {
-    name: newUser.name,
-    email: newUser.email,
-    isAuthenticated: true,
-    id: newUser.id
+    request.checkBody('name', 'Please enter a name.').notEmpty();
+    request.checkBody('email', 'Please enter a valid email address').matches(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, "i");
+    request.checkBody('password', 'Password must be at least 8 characters and contain one letter and one number').matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, "i");
+    request.checkBody('cpwd', 'Passwords must match').equals(password);
+    var errors = request.validationErrors();
+    var existingUser = await models.users.find({ where: { email: email } });
+    if (existingUser) {
+        errors = [{msg: 'That email has already been registered.'}];
+        var model = { errors: errors };
+        response.render('register', model);
+    } else if (errors) {
+        var model = { errors: errors };
+        response.render('register', model);
+    } else {
+        var newUser = await models.users.create({ name: name, email: email, password: password });
+        var userSession = {
+            name: newUser.name,
+            email: newUser.email,
+            isAuthenticated: true,
+            id: newUser.id
+        }
+        request.session.user = userSession;
+
+
+        // response.send(newUser);
+        response.redirect('dashboard')
     }
-    request.session.user = userSession;
-
-
-    // response.send(newUser);
-    response.redirect('dashboard')
-
 })
 
 router.post('/login', async (request, response) => {
@@ -52,7 +67,7 @@ router.post('/login', async (request, response) => {
         response.redirect('dashboard');
     } else {
         var msg = "Invalid Email/Password";
-        var error = {msg: msg};
+        var error = { msg: msg };
         response.render('login', error);
     }
 });
